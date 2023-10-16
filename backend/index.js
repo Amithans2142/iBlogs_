e2d367d60b84965c9cb9dc0d43fcd785fa737ada
Amithans2142 <109544186+Amithans2142@users.noreply.github.com>
2023-10-16 21:43:27 +0530

    hi

--- backend/index.js
+++ backend/index.js
@@ -1,21 +1,29 @@
 const express = require('express');
 const app = express();
 require('dotenv').config();
-const Port = process.env.PORT || 4000 ;
+const Port = process.env.PORT || 4000;
 const cors = require('cors');
-app.use(cors());
 
+const corsOptions = {
+  origin: 'https://frontend-nine-iota.vercel.app', // Replace with your frontend origin
+  methods: 'GET,POST,PUT,DELETE',
+  optionsSuccessStatus: 204, // Some legacy browsers (IE11) choke on 204
+};
 
+app.use(cors(corsOptions));
 
-app.listen(Port, ()=>{
-    console.log("Listening At Port",Port);
-})
+app.listen(Port, () => {
+  console.log("Listening At Port", Port);
+});
+
 app.use(express.json());
 
-const route = require('../backend/routes/route')
-app.use('/api/v1',route);
-app.get("/",(req,res)=> {
-    res.json("hello");
-})
+const route = require('../backend/routes/route');
+app.use('/api/v1', route);
+
+app.get("/", (req, res) => {
+  res.json("hello");
+});
+
 const connectToDb = require('../backend/config/database');
 connectToDb();
